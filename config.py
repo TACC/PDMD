80c97a9f0ab92fb4222acf07c0aa20ca12962572
01Yan <140186842+01Yan@users.noreply.github.com>
2024-11-20 10:46:25 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+from PDMD import get_config
+
+CONFIGS = {
+    'data_config': {
+        'main_path': './',
+        'dataset': 'energy_dataset',
+        'model': 'ChemGNN_energy',
+    },
+    'training_config': {
+        'device_type': 'cpu',
+        'loss_fn_id': 1,
+        'epoch': 2000,
+        'epoch_step': 1,
+        'batch_size': 1024,
+        'lr': 0.002,
+        'seed': 0,
+        'train_length': 0.8,
+        'val_length': 0.2,
+    }
+}
+
+config = get_config(CONFIGS)
