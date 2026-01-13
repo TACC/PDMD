de656bfad7ffd5c0212be37e3c7599dd3c934d8c
Hongyu Yan <140186842+01Yan@users.noreply.github.com>
2026-01-13 22:02:17 +0800

    Update ChemGNN_calculator.py

--- PDMD/benchmark/ChemGNN_calculator.py
+++ PDMD/benchmark/ChemGNN_calculator.py
@@ -29,9 +29,23 @@ class ChemGNN_Calculator(Calculator):
   checkpoint_energy = torch.load(self.energy_pth_filename,
                                  map_location=torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
                                  weights_only=False)
+  
+  if ("min_energy_values" not in checkpoint_energy) or ("max_energy_values" not in checkpoint_energy):
+   if "min_energy_values" not in checkpoint_energy:
+    checkpoint_energy["min_energy_values"] = torch.tensor(np.loadtxt("./PDMD/benchmark/min_values_energy_round4.txt"),
+                                                          dtype=torch.float32)
+   if "max_energy_values" not in checkpoint_energy:
+    checkpoint_energy["max_energy_values"] = torch.tensor(np.loadtxt("./PDMD/benchmark/max_values_energy_round4.txt"),
+                                                          dtype=torch.float32)
+   torch.save(checkpoint_energy, self.energy_pth_filename)
+
   energy_model_state_dict = checkpoint_energy["model_state_dict"]
+  energy_feature_min_values = checkpoint_energy["min_energy_values"]
+  energy_feature_max_values = checkpoint_energy["max_energy_values"]
   self.energy_model.load_state_dict(energy_model_state_dict)
   self.energy_model.CMA = CMA
+  self.energy_feature_min_values = energy_feature_min_values
+  self.energy_feature_max_values = energy_feature_max_values
 
   # load forces .pt model
   self.forces_model = ChemGNN_ForcesModel()
@@ -39,9 +53,23 @@ class ChemGNN_Calculator(Calculator):
   checkpoint_forces = torch.load(self.forces_pth_filename,
                                  map_location=torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
                                  weights_only=False)
+  
+  if ("min_force_values" not in checkpoint_forces) or ("max_force_values" not in checkpoint_forces):
+   if "min_force_values" not in checkpoint_forces:
+    checkpoint_forces["min_force_values"] = torch.tensor(np.loadtxt("./PDMD/benchmark/min_values_force_round4.txt"),
+                                                         dtype=torch.float32)
+   if "max_force_values" not in checkpoint_forces:
+    checkpoint_forces["max_force_values"] = torch.tensor(np.loadtxt("./PDMD/benchmark/max_values_force_round4.txt"),
+                                                         dtype=torch.float32)
+   torch.save(checkpoint_forces, self.forces_pth_filename)
+
   forces_model_state_dict = checkpoint_forces["model_state_dict"]
+  forces_feature_min_values = checkpoint_forces["min_force_values"]
+  forces_feature_max_values = checkpoint_forces["max_force_values"]
   self.forces_model.load_state_dict(forces_model_state_dict)
   self.forces_model.CMA = CMA
+  self.forces_feature_min_values = forces_feature_min_values
+  self.forces_feature_max_values = forces_feature_max_values
 
  def __del__(self):
   #delete energy model
@@ -65,8 +93,8 @@ class ChemGNN_Calculator(Calculator):
 
   #convert positions to a torch tensor
   tensor_positions = torch.tensor(positions)
-  energy = self.energy_model(atomic_numbers,tensor_positions)
-  forces = self.forces_model(atomic_numbers,tensor_positions)
+  energy = self.energy_model(atomic_numbers,tensor_positions, self.energy_feature_min_values, self.energy_feature_max_values)
+  forces = self.forces_model(atomic_numbers,tensor_positions, self.forces_feature_min_values, self.forces_feature_max_values)
 
   #energy and forces unit conversion
   #the units for energy and forces in machine learning are Hartree and Hartree/Bohr, respectively
