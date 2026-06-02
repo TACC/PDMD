084afa843472694b8cc284a0b1fd04b4efada7f5
taccchen <hchen@tacc.utexas.edu>
2026-06-02 16:27:25 -0400

    Update the PDMD/models folder

--- PDMD/models/ChemGNN_energy.py
+++ PDMD/models/ChemGNN_energy.py
@@ -10,174 +10,7 @@ from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.nn.dense.linear import Linear
 from torch_geometric.typing import Adj, OptTensor, Size
 from torch_geometric.nn.inits import reset
-
-
-
-class CEALConv(MessagePassing):
-    r"""Chemical Environment Adaptive Learning
-    from the `"Chemical Environment Adaptive Learning for Optical Band Gap Prediction of Doped Graphitic Carbon Nitride Nanosheets"
-
-
-
-    Args:
-        in_channels (int): Size of each input sample, or :obj:`-1` to derive
-            the size from the first input(s) to the forward method.
-        out_channels (int): Size of each output sample.
-        aggregators (list of str): Set of aggregation function identifiers,
-            namely :obj:`"sum"`, :obj:`"mean"`, :obj:`"min"`, :obj:`"max"`,
-            :obj:`"var"` and :obj:`"std"`.
-        edge_dim (int, optional): Edge feature dimensionality (in case
-            there are any). (default :obj:`None`)
-        towers (int, optional): Number of towers (default: :obj:`1`).
-        pre_layers (int, optional): Number of transformation layers before
-            aggregation (default: :obj:`1`).
-        post_layers (int, optional): Number of transformation layers after
-            aggregation (default: :obj:`1`).
-        divide_input (bool, optional): Whether the input features should
-            be split between towers or not (default: :obj:`False`).
-        **kwargs (optional): Additional arguments of
-            :class:`torch_geometric.nn.conv.MessagePassing`.
-
-    """
-
-    def __init__(self, in_channels: int, out_channels: int, weights: Tensor,
-                 aggregators: List[str],edge_dim: Optional[int] = None, towers: int = 1,
-                 pre_layers: int = 1, post_layers: int = 1,
-                 divide_input: bool = False, **kwargs):
-
-        kwargs.setdefault('aggr', None)
-        super().__init__(node_dim=0, **kwargs)
-
-        if divide_input:
-            assert in_channels % towers == 0
-        assert out_channels % towers == 0
-
-        device = torch.device("cuda" if torch.cuda.is_available() and 'cuda' else "cpu")
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.aggregators = aggregators
-        self.edge_dim = edge_dim
-        self.towers = towers
-        self.divide_input = divide_input
-        self.weights = weights
-
-        self.F_in = in_channels // towers if divide_input else in_channels
-        self.F_out = self.out_channels // towers
-
-
-        if self.edge_dim is not None:
-            self.edge_encoder = Linear(edge_dim, self.F_in)
-
-        self.pre_nns = ModuleList()
-        self.post_nns = ModuleList()
-        for _ in range(towers):
-            modules = [Linear((3 if edge_dim else 2) * self.F_in, self.F_in)]
-            for _ in range(pre_layers - 1):
-                modules += [ReLU()]
-                modules += [Linear(self.F_in, self.F_in)]
-            self.pre_nns.append(Sequential(*modules))
-
-            in_channels = (len(aggregators) + 1) * self.F_in
-            modules = [Linear(in_channels, self.F_out)]
-            for _ in range(post_layers - 1):
-                modules += [ReLU()]
-                modules += [Linear(self.F_out, self.F_out)]
-            self.post_nns.append(Sequential(*modules))
-
-        self.lin = Linear(out_channels, out_channels)
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        if self.edge_dim is not None:
-            self.edge_encoder.reset_parameters()
-        for nn in self.pre_nns:
-            reset(nn)
-        for nn in self.post_nns:
-            reset(nn)
-        self.lin.reset_parameters()
-
-    def forward(self, x: Tensor, edge_index: Adj, weights: Tensor,
-                edge_attr: OptTensor = None
-                ) -> Tensor:
-        """"""
-
-        if self.divide_input:
-            x = x.view(-1, self.towers, self.F_in)
-        else:
-            x = x.view(-1, 1, self.F_in).repeat(1, self.towers, 1)
-
-        # propagate_type: (x: Tensor, edge_attr: OptTensor, weights: Tensor)
-        out = self.propagate(edge_index, x=x, edge_attr=edge_attr, weights=weights, size=None)
-
-        out = torch.cat([x, out], dim=-1)
-        outs = [nn(out[:, i]) for i, nn in enumerate(self.post_nns)]
-        out = torch.cat(outs, dim=1)
-
-        return self.lin(out)
-
-    def message(self, x_i: Tensor, x_j: Tensor,
-                edge_attr: OptTensor) -> Tensor:
-
-        h: Tensor = x_i
-        if edge_attr is not None:
-            edge_attr = self.edge_encoder(edge_attr)
-            edge_attr = edge_attr.view(-1, 1, self.F_in)
-            edge_attr = edge_attr.repeat(1, self.towers, 1)
-            h = torch.cat([x_i, x_j, edge_attr], dim=-1)
-        else:
-            h = torch.cat([x_i, x_j], dim=-1)
-
-        hs = [nn(h[:, i]) for i, nn in enumerate(self.pre_nns)]
-        return torch.stack(hs, dim=1)
-
-    def aggregate(self, inputs: Tensor, index: Tensor, weights: Tensor,
-                  dim_size: Optional[int] = None,
-                  ) -> Tensor:
-
-        device = torch.device("cuda" if torch.cuda.is_available() and 'cuda' else "cpu")
-        outs = []
-
-        #the second and third dimension sizes of the inputs tensor
-        ydim_inputs = inputs.size(dim=1)
-        zdim_inputs = inputs.size(dim=2)
-        #expand the one-dimensional index tensor to the same size as the three-dimensional inputs tensor
-        #note that pytorch_scatter_reduce requires the same dimension for the index and inputs tesors
-        tensor_index = index.unsqueeze(-1).unsqueeze(-1).expand(inputs.size())
-
-        for aggregator in self.aggregators:
-            if aggregator == 'sum':
-                out = torch.zeros(dim_size, ydim_inputs, zdim_inputs, dtype=inputs.dtype).to(device)
-                out = weights[0] * out.scatter_reduce(dim=0, index=tensor_index, src=inputs, reduce="sum", include_self=False)
-            elif aggregator == 'mean':
-                out = torch.zeros(dim_size, ydim_inputs, zdim_inputs, dtype=inputs.dtype).to(device)
-                out = weights[1] * out.scatter_reduce(dim=0, index=tensor_index, src=inputs, reduce="mean", include_self=False)
-            elif aggregator == 'min':
-                out = torch.zeros(dim_size, ydim_inputs, zdim_inputs, dtype=inputs.dtype).to(device)
-                out = weights[2] * out.scatter_reduce(dim=0, index=tensor_index, src=inputs, reduce="amin", include_self=False)
-            elif aggregator == 'max':
-                out = torch.zeros(dim_size, ydim_inputs, zdim_inputs, dtype=inputs.dtype).to(device)
-                out = weights[3] * out.scatter_reduce(dim=0, index=tensor_index, src=inputs, reduce="amax", include_self=False)
-            elif aggregator == 'var' or aggregator == 'std':
-                mean =  torch.zeros(dim_size, ydim_inputs, zdim_inputs, dtype=inputs.dtype).to(device)
-                mean = mean.scatter_reduce(dim=0, index=tensor_index, src=inputs, reduce="mean", include_self=False)
-                mean_squares = torch.zeros(dim_size, ydim_inputs, zdim_inputs, dtype=inputs.dtype).to(device)
-                mean_squares = mean_squares.scatter_reduce(dim=0, index=tensor_index, src=inputs * inputs, reduce="mean", include_self=False)
-                out = (mean_squares - mean * mean)
-                if aggregator == 'std':
-                    out = weights[4] * torch.sqrt(torch.relu(out) + 1e-5)
-            else:
-                raise ValueError(f'Unknown aggregator "{aggregator}".')
-            outs.append(out)
-        out = torch.cat(outs, dim=-1)
-
-        outs = []
-        outs.append(out)
-        return torch.cat(outs, dim=-1)
-
-    def __repr__(self):
-        return (f'{self.__class__.__name__}({self.in_channels}, '
-                f'{self.out_channels}, towers={self.towers}, '
-                f'edge_dim={self.edge_dim})')
+from PDMD.models.HyperCEALConv import HyperCEALConv
 
 class ENERGY_Model(torch.nn.Module):
     def __init__(self):
@@ -189,18 +22,22 @@ class ENERGY_Model(torch.nn.Module):
         self.out_num = 20
         aggregators = ['sum', 'mean', 'min', 'max', 'std']
         self.weights = torch.nn.Parameter(torch.rand(len(aggregators)))
+        self.hyperedge_order = 2
+        self.hyperedge_weights = torch.nn.Parameter(torch.rand(self.hyperedge_order - 1))
         self.convs = ModuleList()
         self.batch_norms = ModuleList()
 
         for _ in range(self.conv_num):
             if _ == 0:
-                conv = CEALConv(in_channels=self.in_num, out_channels=self.mid_num, weights=self.weights,
+                conv = HyperCEALConv(in_channels=self.in_num, out_channels=self.mid_num, weights=self.weights,
                                 aggregators=aggregators, edge_dim=10, towers=1, pre_layers=3, post_layers=3,
+                                hyperedge_order=self.hyperedge_order, hyperedge_weights=self.hyperedge_weights,
                                 divide_input=False)
                 norms = BatchNorm(self.mid_num)
             else:
-                conv = CEALConv(in_channels=self.mid_num, out_channels=self.out_num, weights=self.weights,
+                conv = HyperCEALConv(in_channels=self.mid_num, out_channels=self.out_num, weights=self.weights,
                                 aggregators=aggregators, edge_dim=10, towers=1, pre_layers=3, post_layers=3,
+                                hyperedge_order=self.hyperedge_order, hyperedge_weights=self.hyperedge_weights,
                                 divide_input=False)
                 norms = BatchNorm(self.out_num)
             self.convs.append(conv)
@@ -210,17 +47,17 @@ class ENERGY_Model(torch.nn.Module):
         self.energy_predictor = Sequential(Linear(self.out_num, 100), ReLU(), Linear(100, 10), ReLU(), Linear(10, 1))
 
     def forward(self, input_dict):
-        assert {"x", "edge_index", "edge_attr", "batch"}.issubset(input_dict.keys())
-        node_attr, edge_index, edge_attr, batch = iter([input_dict.get(one_key) for one_key in ["x", "edge_index", "edge_attr", "batch"]])
-        edge_attr = edge_attr.unsqueeze(-1)
+        assert {"x", "batch", "hyperedge_index", "hyperedge_attr"}.issubset(input_dict.keys())
+        node_attr, batch, hyperedge_index, hyperedge_attr = iter([input_dict.get(one_key) for one_key in ["x", "batch", "hyperedge_index", "hyperedge_attr"]])
+
+        hyperedge_attr = hyperedge_attr.unsqueeze(-1)
         if torch.get_default_dtype() == torch.float64:
             node_attr = node_attr.to(torch.float64)
-            edge_attr = edge_attr.to(torch.float64)
-
+            hyperedge_attr = hyperedge_attr.to(torch.float64)
         node_attr = self.node_embedding(node_attr)
-        edge_attr = self.edge_embedding(edge_attr)
+        hyperedge_attr = self.edge_embedding(hyperedge_attr)
         for conv, batch_norm in zip(self.convs, self.batch_norms):
-            node_attr = F.relu(batch_norm(conv(node_attr, edge_index, self.weights, edge_attr)))
+            node_attr = F.relu(batch_norm(conv(node_attr, hyperedge_index, self.weights, hyperedge_attr)))
 
         energy = global_add_pool(node_attr, batch)
         energy = self.energy_predictor(energy)
